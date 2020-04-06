3c88541255fdf61a482599ae820c61373592fd7e
John Sermarini <JohnSermarini@knights.ucf.edu>
2020-04-05 20:35:59 -0400

    Added setting color and size settings to GUI

--- pte.py
+++ pte.py
@@ -16,14 +16,9 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-resize_width = 30
-resize_height = 30
-num_colors = 16
 column_size = 2.8 # This number is about 20 pixels, same as the default height
 cell_fill_type = 'solid'
 legend_buffer = 1
-use_dmc = True
-file_path = ""
 
 
 # TODO check out openpyxl.utils.cell.get_column_letter(idx)
@@ -48,19 +43,34 @@ file_path = ""
 TODO error checks:
 Is file open in excel during save.
 is path less than 5 characters
+Pic has too many colors?????
 """
 
 
 def main(argv):
 	# Init
+	window = tk.Tk()
+	guivar_checkbox_use_dmc = tk.IntVar()
 
 	# Configure GUI
-	window = tk.Tk()
 	window.title("CSX")
 	window.geometry("300x300")
 	window.configure(background="white")
-	tk.Button(window, text="Select File", width=100, command=lambda : user_select_file()).pack()
-	tk.Button(window, text="Create", width=100, command=lambda : create()).pack()
+	tk.Label(window, text="Width [1 - 99]:").pack()
+	entry_width = tk.Entry(window, width=100)
+	entry_width.pack()
+	tk.Label(window, text="Height [1 - 99]:").pack()
+	entry_height = tk.Entry(window, width=100)
+	entry_height.pack()
+	tk.Label(window, text="Number of Colors [4 - 16]:").pack()
+	entry_num_colors = tk.Entry(window, width=100)
+	entry_num_colors.pack()
+	checkbox_use_dmc_colors = tk.Checkbutton(text="Use DMC color palette", variable=guivar_checkbox_use_dmc, onvalue=True, offvalue=False)
+	checkbox_use_dmc_colors.pack()
+	button_select_file = tk.Button(window, text="Select File", width=100, command=lambda : user_select_file())
+	button_select_file.pack()
+	button_create = tk.Button(window, text="Create", width=100, command=lambda : create(guivar_checkbox_use_dmc.get(), entry_width.get(), entry_height.get(), entry_num_colors.get()))
+	button_create.pack()
 	window.mainloop()
 
 
@@ -87,6 +97,34 @@ def read_image(file_name):
 
 		return None
 
+
+"""
+#############################################
+# IN: PIL image
+# OUT: 2D array with each value containing a rgb tuple
+# OUT: 2D array with each value containing an int representing the color map value
+#############################################
+def get_colors(image):
+	used_colors = []
+	colors = []
+	color_map = []
+
+	for x in range(0, image.size[0]): # Left column to right column
+		column_colors = []
+		column_map = []
+		for y in range(0, image.size[1]): # Top row to bottom row
+			pixel_color = image.getpixel((x,y))
+			if(pixel_color not in used_colors):
+				used_colors.append(pixel_color)
+			pixel_map = used_colors.index(pixel_color)
+
+			column_colors.append(pixel_color)
+			column_map.append(pixel_map)
+		colors.append(column_colors)
+		color_map.append(column_map)
+
+	return colors, color_map
+"""
 
 #############################################
 # IN: PIL image
@@ -166,16 +204,19 @@ def reduce_color_palette(image, num_colors):
 # IN:
 # OUT: 2D array with each value containing a rgb tuple
 #############################################
-def convert_colors_to_dmc(colors, color_map):
+def convert_colors_to_dmc(colors, color_map, num_colors):
 	#converted_colors = np.full(len(colors), -1, dtype=object)
 	converted_colors = []
-	for i in range(0, len(color_map)):
+	for i in range(0, num_colors):
 		converted_colors.append((-1, -1, -1))
 
 	for x in range(0, len(colors)):
 		print("Converting - " +  str(x) + "/" + str(len(colors)) + " to DMC color palette")
 		for y in range(0, len(colors[x])):
 			map_value = color_map[x][y]
+			print(map_value)
+			print("Len: " + str(len(converted_colors)))
+			print("Color Map Len: " + str(len(color_map)))
 			if(converted_colors[map_value] == (-1, -1, -1)): # converted color not set
 				converted_colors[map_value] = find_closest_dmc_color(colors[x][y])
 				print("Converted " + str(colors[x][y]) + " to " + str(converted_colors[map_value]))
@@ -235,7 +276,7 @@ def get_used_color_palette(colors, color_map):
 	return used_colors, used_map
 
 
-def get_dmc_name(color_rgb):
+def get_dmc_name(use_dmc, color_rgb):
 	if use_dmc:
 		#TODO
 		return "TODO"
@@ -260,22 +301,34 @@ def user_select_file():
 	print("File:", file_path)
 
 
-def create():
+def create(use_dmc, width, height, num_colors):
+	# Grab file_path
 	global file_path
+
+	# Check inputs for errors
 	if not file_path_valid(file_path):
 		return
+	if not dimensions_valid(width, height):
+		return
+	if not num_colors_valid(num_colors):
+		return
+
+	# Init
 	file_name = get_file_name_from_path(file_path)
+	width = int(width)
+	height = int(height)
+	num_colors = int(num_colors)
 
 	# Get image from file
 	image = read_image(file_path)
-	image = adjust_image_size(image, resize_width, resize_height)
+	image = adjust_image_size(image, width, height)
 	image = trim_image(image)
 
 	# Get colors from image
 	image = reduce_color_palette(image, num_colors)
 	colors, color_map = get_colors(image)
 	if use_dmc:
-		colors = convert_colors_to_dmc(colors, color_map)
+		colors = convert_colors_to_dmc(colors, color_map, num_colors)
 
 	# Create worksheet
 	wb = Workbook()
@@ -315,7 +368,7 @@ def create():
 		color_hex = rgb_to_hex(color_rgb)
 		ws[get_cell_name(image.width + legend_buffer, c + 1)].fill = styles.PatternFill(fill_type=cell_fill_type, start_color=color_hex, end_color=color_hex)
 		ws[get_cell_name(image.width + legend_buffer, c + 1)].value = str(color_symbol)
-		ws[get_cell_name(image.width + legend_buffer + 1, c + 1)].value = get_dmc_name(color_rgb)
+		ws[get_cell_name(image.width + legend_buffer + 1, c + 1)].value = get_dmc_name(use_dmc, color_rgb)
 		ws[get_cell_name(image.width + legend_buffer + 2, c + 1)].value = str(color_hex)
 		ws[get_cell_name(image.width + legend_buffer + 3, c + 1)].value = str(color_rgb[0])
 		ws[get_cell_name(image.width + legend_buffer + 4, c + 1)].value = str(color_rgb[1])
@@ -327,21 +380,52 @@ def create():
 	wb.save(output_directory + output_file_name)
 	print(output_file_name + " created")
 
+	# Close
 	image.close()
 
 
 def file_path_valid(file_path):
 	# Check for empty path
-	if(file_path == ""):
+	if file_path == "":
 		print("Error: Path file path empty.")
 		return False
 	# Check file type
 	file_extension = file_path[-5:].lower()
-	print(file_extension)
 	if  ".jpg" not in file_extension.lower() and \
 		".png" not in file_extension.lower() and \
 		".jpeg" not in file_extension.lower() :
 		print("Error: File must be type '.png' or '.jpg'")
+		return False
+	return True
+
+
+def dimensions_valid(width, height):
+	# Check if eheight and width are numbers
+	if not width.isnumeric():
+		print("Error: Width contains non-numeric characters.")
+		return False
+	if not height.isnumeric():
+		print("Error: Height contains non-numeric characters.")
+		return False
+	# Check if height in width are within the desired range
+	width = int(width)
+	height = int(height)
+	if width < 1 or width > 99:
+		print("Error: Width '" + str(width) + "' not valid. Must be between 1 and 99.")
+		return False
+	if height < 1 or height > 99:
+		print("Error: Height '" + str(height) + "' not valid. Must be between 1 and 99.")
+		return False
+	return True
+
+
+def num_colors_valid(num_colors):
+	if not num_colors.isnumeric():
+		print("Error: Number of Colors contains non-numeric characters.")
+		return False
+	num_colors = int(num_colors)
+	if num_colors < 4 or num_colors > 16:
+		print("Error: Number of Colors '" + str(num_colors) + "' not valid. Must be between 4 and 16.")
 		return False
 	return True
 
